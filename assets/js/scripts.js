130f984702c35e2810729b0a631cbe47d1059c31
Nataliya Zhuravleva <natalindria@gmail.com>
2020-10-27 16:40:51 -0700

    Create function and alert. Doesn't work.
    Co-authored-by: Chelsea Becker <cschweig2@gmail.com>

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,13 +1,33 @@
 // Business logic:
 
+function factorial1(n) {
 
+  if (n == 0) {
+    return 1;
+  } else
+  n * factorial1(n - 1);
+}
 
 // User interface logic:
 
 $(document).ready(function () {
+  $("button#factorial").click(function () {
+    
+    // let n = 5;
+    // let factorial = n;
 
+    // for (let i = n - 1; i > 0; i--) {  
+    //  factorial *= i;
+    //  console.log(factorial);
 
+    //}
+    let result = factorial1(5);
+   // console.log(result);
+    alert(result);
+    
+    //
+   // event.preventDefault();
+  });
 
-  event.preventDefault();
 
 });
