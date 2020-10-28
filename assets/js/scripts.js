0fe198e26b22e07c48d90e68f38cd77d57f51c12
Nataliya Zhuravleva <natalindria@gmail.com>
2020-10-27 20:03:04 -0700

    Add check > 0, empty field.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,26 +5,30 @@ function factorial1(n) {
   if (n == 0) {
     return 1;
   } else
-  return n * factorial1(n - 1);
+    return n * factorial1(n - 1);
 }
 
 // User interface logic:
 
 $(document).ready(function () {
   $("button#factorial").click(function (event) {
-    
+    //without recursion
     // let n = 5;
     // let factorial = n;
-
     // for (let i = n - 1; i > 0; i--) {  
     //  factorial *= i;
     //  console.log(factorial);
+    // }
+    //with recursion
 
-    //}
-    let result = factorial1(5);
-   // console.log(result);
-    alert(result);
-    
+    const input = parseInt($("#input").val());
+    if (input && input>=0) {
+      let result = factorial1(input);
+      alert(result);
+      
+    } else {
+      $("#warning").show();
+    }
     //
     event.preventDefault();
   });
